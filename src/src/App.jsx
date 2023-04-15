43ed899d1462b72d2b128d5f88b7006b53177b10
Neelesh Joshi <80151802+Neeltyper001@users.noreply.github.com>
2023-04-15 15:30:33 +0530

    Br 1 (#1)
    * Branch Initial Commit
    
    * Project Files uploaded
    
    * Updated the readme
    
    * dummy file
    
    * Added mp4 file
    
    * deleting  mp4 file
    
    * fd
    
    * Deleted the typo mistaken directory
    
    * Added mp4 file
    
    * Deleted help.txt
    
    * Added meme-generator.png
    
    * Add files via upload
    
    * Added the SS
    
    * Updated Readme
    
    Added few Markdowns:
    - Added the SS, added about, motivation, made with, future vision, Link heading
    - Overall the documentation for the readme has been improved

---
+++ src/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/HeaderDir/Header';
+import './components/HeaderDir/index.css'
+import Main from './components/MainDir/Main';
+import './components/MainDir/index.css'
+import './components/FormDir/media-queries.css'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/src/components/HeaderDir/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header className = "header">
+            <img className="image" src="/meme-assets/troll.gif" height="100px" width="100px"></img>
+            <h2 className = "heading">MemeGenerator</h2>
+            <div className = "quoteBox">
+                <p className = "quote">-by a legend</p>
+                <q className = "quote">If real life pains are dart, memes are the art</q>
+            </div>
+        </header>
+    );
+} 
